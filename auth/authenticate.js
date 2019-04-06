dfe293972a444205b32c5437969cb122f431f457
ltann <leeroyleeroy18@gmail.com>
2019-04-06 10:38:55 -0700

    Finished Answers. Register and Login function completed

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 const jwtKey =
-  process.env.JWT_SECRET ||
+  process.env.JWT_SECRET || 'secret password'
   'add a .env file to root of project with the JWT_SECRET variable';
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  jwtKey
 };
 
 // implementation details
--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,71 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const { authenticate } = require('../auth/authenticate');
+const Users = require('./routes-model')
+
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/api/users', getUsers)
 };
+
+function getUsers(req, res) {
+  Users.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(error => res.send(error))
+}
 
 function register(req, res) {
   // implement user registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({ token, message: `Welcome young dragon ${user.username}`})
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'oops'})
+    })
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, jwtKey, options)
 }
 
 function getJokes(req, res) {
