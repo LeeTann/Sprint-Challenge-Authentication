42ed518949db2f4654db23988f794a760253a3a0
ltann <leeroyleeroy18@gmail.com>
2019-04-06 12:00:31 -0700

    Connect to React. Added signup, signin, and jokes connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom'
+import { Navbar, Button, Container } from 'reactstrap'
+
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import Jokes from './components/Jokes'
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <p>Testing</p>
+       <Navbar>
+         <NavLink to="/signup" className="link">SignUp</NavLink>
+         <NavLink to="/signin" className="link">SignIn</NavLink>
+         <NavLink to="/jokes" className="link">Jokes</NavLink>
+         <Button onClick={this.logout}>Logout</Button>
+       </Navbar>
+       <Container>
+         <Route path="/signup" component={Signup} />
+         <Route path="/signin" component={Signin} />
+         <Route path="/jokes" component={Jokes} />
+       </Container>
       </div>
     );
   }
+
+  logout = () => {
+    localStorage.removeItem('jwt')
+  
+    this.props.history.push('/login')
+  }
 }
 
-export default App;
+export default withRouter(App);
